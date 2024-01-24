6e6aabca970fda0a4d893ea4b005bd2268c499b1
Federico Lo Dolce <flodolce92@gmail.com>
2024-01-24 04:36:25 +0100

    delete comments

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: flo-dolc <flo-dolc@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 02:04:52 by flo-dolc          #+#    #+#             */
-/*   Updated: 2024/01/20 05:25:42 by flo-dolc         ###   ########.fr       */
+/*   Updated: 2024/01/24 04:28:21 by flo-dolc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,47 +64,3 @@ int	ft_printf(const char *str, ...)
 	va_end(args);
 	return (len);
 }
-
-// int	main(void)
-// {
-// 	int tmp;
-// 	char *str = NULL;
-
-// 	printf("\tlen: %d\n", ft_printf("%c", 'a'));
-// 	tmp = printf("%c\t", 'a');
-// 	printf("lenOR: %d\n\n", tmp - 1);
-
-// 	printf("\tlen: %d\n", ft_printf("%s", str));
-// 	tmp = printf("%s\t", str);
-// 	printf("lenOR: %d\n\n", tmp - 1);
-
-// 	printf("\tlen: %d\n", ft_printf("%p", str));
-// 	tmp = printf("%p\t", str);
-// 	printf("lenOR: %d\n\n", tmp - 1);
-
-// 	printf("\tlen: %d\n", ft_printf("%d", 42));
-// 	tmp = printf("%d\t", 42);
-// 	printf("lenOR: %d\n\n", tmp - 1);
-
-// 	printf("\tlen: %d\n", ft_printf("%i", 42));
-// 	tmp = printf("%i\t", 42);
-// 	printf("lenOR: %d\n\n", tmp - 1);
-
-// 	printf("\tlen: %d\n", ft_printf("%u", 4294967295U));
-// 	tmp = printf("%u\t", 4294967295U);
-// 	printf("lenOR: %d\n\n", tmp - 1);
-
-// 	printf("\tlen: %d\n", ft_printf("%x", 1000));
-// 	tmp = printf("%x\t", 1000);
-// 	printf("lenOR: %d\n\n", tmp - 1);
-
-// 	printf("\tlen: %d\n", ft_printf("%X", -1000));
-// 	tmp = printf("%X\t", -1000);
-// 	printf("lenOR: %d\n\n", tmp - 1);
-
-// 	printf("\tlen: %d\n", ft_printf("%%"));
-// 	tmp = printf("%%\t");
-// 	printf("lenOR: %d\n\n", tmp - 1);
-
-// 	return (0);
-// }
